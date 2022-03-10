6e33018bf45b0f59c00ae4696eccd35a816d7471
Oscar Medina <me@devopsoscar.dev>
2022-03-09 19:31:58 -0800

    feature: adding ngnix add-on for kubernetes (#2)
    * feature: adding ngnix add-on for kubernetes
    
    * terraform-docs: automated action
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- examples/eks-cluster-with-argocd/main.tf
+++ examples/eks-cluster-with-argocd/main.tf
@@ -191,7 +191,7 @@ module "kubernetes-addons" {
   enable_aws_load_balancer_controller = true
   enable_cert_manager                 = true
   enable_cluster_autoscaler           = true
-  enable_ingress_nginx                = false
+  enable_ingress_nginx                = true
   enable_karpenter                    = false
   enable_keda                         = false
   enable_metrics_server               = false
